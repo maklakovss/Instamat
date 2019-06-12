33fe9fecd83c1461622e2c4e7d8be10bc09e13b1
maklakovss <maklakovss@gmail.com>
2019-06-12 13:16:44 +0500

    unit test

---
+++ app/src/main/java/com/mss/instamat/tests/unit/Adder.java
@@ -0,0 +1,7 @@
+package com.mss.instamat.tests.unit;
+
+public class Adder {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ app/src/test/java/com/mss/instamat/tests/unit/AdderTest.java
@@ -0,0 +1,33 @@
+package com.mss.instamat.tests.unit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class AdderTest {
+
+    private Adder adder;
+
+    @Before
+    public void setUp() throws Exception {
+        adder = new Adder();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        adder = null;
+    }
+
+    @Test
+    public void twoAndThree_add_isCorrect() {
+        assertEquals(adder.add(2, 3), 5);
+    }
+
+    @Test
+    public void twoAndThree_add_isIncorrect() {
+        assertNotEquals(adder.add(2, 3), 4);
+    }
+}
